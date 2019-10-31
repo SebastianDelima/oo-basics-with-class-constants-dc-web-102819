1d244e4d001c45317ebbfe83357358fb9fafb450
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-10-31 14:17:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 GENRES = []
   def initialize(title)
     @title = title
