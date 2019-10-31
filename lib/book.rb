177d399e82837263eea9aca075fad576226ec161
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-10-31 14:10:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,15 @@
+require 'pry'
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
-
+GENRES = []
   def initialize(title)
     @title = title
   end
-
+  
+def genre=(genre)
+  GENRES << genre
+end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
